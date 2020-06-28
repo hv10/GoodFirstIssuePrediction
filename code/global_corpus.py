b19399e9ca4892d44d87c0f6fbe5c1f6cfd861ca
hv10 <noeldanz@gmail.com>
2020-06-28 23:11:18 +0200

    WIP: start working on comment acquisition.

--- code/global_corpus.py
+++ code/global_corpus.py
@@ -1,14 +1,37 @@
+import os
+import sys
 from pathlib import Path
 from github import Github
+from dotenv import load_dotenv, find_dotenv
 
-def build_global_corpus(repo_list=[]):
-    pass
 
-def build_global_corpus_from_file(path=(Path.cwd() / "global_corpus.repos")):
+def build_global_corpus(repo_list=[], access_token=None):
+    if not access_token:
+        raise ValueError("No GitHub API access token provided.")
+    gh = Github(access_token)
+    for repo_name in repo_list:
+        repo = gh.get_repo(repo_name)
+        issues = [issue for issue in repo.get_issues()]
+        print(len(issues))
+        print(issues[1].comments_url)
+
+
+def build_global_corpus_from_file(
+    path=(Path.cwd() / "global_corpus.repos"), access_token=None
+):
     with open(path, mode="r") as f:
-        repo_list = f.readlines()
-        print(repo_list)
-        build_global_corpus(repo_list)
+        repo_list = [repo.strip() for repo in f.readlines()]
+        build_global_corpus(repo_list, access_token)
 
-if __name__=="__main__":
-    build_global_corpus_from_file()
+
+def main():
+    load_dotenv(find_dotenv())
+    access_token = os.getenv("GITHUB_ACCESS_TOKEN")
+    if len(sys.argv) > 1:
+        build_global_corpus_from_file(sys.argv[1], access_token)
+    else:
+        build_global_corpus_from_file()
+
+
+if __name__ == "__main__":
+    main()
