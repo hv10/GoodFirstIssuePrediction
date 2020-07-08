2b9748288fbb14745389b7fba4088e9fb8532a39
hv10 <noeldanz@gmail.com>
2020-07-08 12:21:17 +0200

    add in collection of all issue texts w/ closed status and comments

--- code/global_corpus.py
+++ code/global_corpus.py
@@ -1,19 +1,53 @@
+import logging
 import os
 import sys
+import yaml
 from pathlib import Path
 from github import Github
 from dotenv import load_dotenv, find_dotenv
 
 
-def build_global_corpus(repo_list=[], access_token=None):
+def ensure_repo_corpus_folder(
+    repo_name: str, corpus_path=(Path.cwd() / "corpus")
+):
+    Path(corpus_path / repo_name).mkdir(parents=True, exist_ok=True)
+
+
+def build_global_corpus(
+    repo_list=[],
+    access_token=None,
+    corpus_path=(Path.cwd() / "corpus"),
+    override=False,
+):
     if not access_token:
         raise ValueError("No GitHub API access token provided.")
     gh = Github(access_token)
     for repo_name in repo_list:
+        logging.info(msg=f"Working on {repo_name}...")
+        ensure_repo_corpus_folder(repo_name, corpus_path=corpus_path)
         repo = gh.get_repo(repo_name)
         issues = [issue for issue in repo.get_issues()]
-        print(len(issues))
-        print(issues[1].comments_url)
+        issue_dump = {"comments": []}
+        for issue in issues:
+            logging.info(msg=f"Collecting issue n.{issue.id}")
+            issue_dump["title"] = issue.title
+            issue_dump["body"] = issue.body
+            issue_dump["closed_at"] = issue.closed_at
+            if issue.comments > 0:
+                for comment in issue.get_comments():
+                    issue_dump["comments"].append(
+                        {
+                            "body": comment.body,
+                            "created_at": comment.created_at,
+                            "user_name": comment.user.login,
+                        }
+                    )
+            issue_dump_path = corpus_path / repo_name / f"issue{issue.id}.yaml"
+            if not Path(issue_dump_path).exists() or override:
+                logging.info(msg=f"Writing issue n.{issue.id}")
+                with open(issue_dump_path, mode="w+") as f:
+                    f.write(yaml.dump(issue_dump))
+        print("\n" + "-" * 20 + "\n")
 
 
 def build_global_corpus_from_file(
@@ -34,4 +68,9 @@ def main():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[logging.FileHandler("debug.log"), logging.StreamHandler()],
+    )
     main()
