8d9e90d5fda0acf2c2a36bf4023c2d5b142a32b2
hv10 <noeldanz@gmail.com>
2020-08-17 22:00:41 +0200

    vectorizer now has a CLI

--- experiments/algorithms/vectorization.py
+++ experiments/algorithms/vectorization.py
@@ -1,5 +1,7 @@
+import argparse
 import logging
 import random
+import experiments.logging_setup
 
 import tensorflow as tf
 from pathlib import Path
@@ -57,11 +59,22 @@ def make_vektorizer(vocab, max_features=10000, max_len=None, ngrams_size=None):
     return Model(inputs=[input], outputs=[output])
 
 
-def main():
-    directory = Path().resolve().parent / "corpus"
-    issues = [str(e.resolve()) for e in directory.glob("**/*.yaml")]
+def main(
+    corpus_dir=Path().resolve().parent / "corpus",
+    sample_size=20000,
+    vocab_size=10000,
+    output_dir=Path(__file__).parent / "vectorizer_model_20000",
+):
+    if not corpus_dir.is_dir():
+        raise ValueError("corpus_dir needs to be a directory")
+    if output_dir is None:
+        output_dir = (
+            Path(__file__).parent
+            / f"vectorizer_model_s{sample_size}_vc{vocab_size}"
+        )
+    issues = [str(e.resolve()) for e in corpus_dir.glob("**/*.yaml")]
     small_issues = random.sample(
-        issues, 20000
+        issues, sample_size
     )  # 33samples/min --> 6000samples in 3h
     df = tf.data.Dataset.from_generator(
         yaml_issue_generator,
@@ -70,11 +83,42 @@ def main():
         output_shapes=(),
     )
     logging.info("Start building vectorization model...")
-    model = make_vektorizer(df.padded_batch(16))
+    logging.info(
+        "This *will* take a while and not give any feedback on progress, sadly we cannot change that."
+    )
+    model = make_vektorizer(df.padded_batch(16), max_features=vocab_size)
     logging.info("Saving...")
-    model.save(str(Path(__file__).parent / "vectorizer_model_20090"))
+    model.save(str(output_dir))
 
 
 if __name__ == "__main__":
-    main()
-    # TODO: needs cli
+    parser = argparse.ArgumentParser(
+        description="I build a vectorizer with a vocabulary of 10.000 words from a body of issues."
+    )
+    parser.add_argument(
+        "corpus_dir", help="corpus directory path", type=Path,
+    )
+    parser.add_argument(
+        "-s",
+        "--sample_size",
+        default=20000,
+        required=False,
+        type=int,
+        help="size of the sample of the whole corpus to be used for training",
+    )
+    parser.add_argument(
+        "-vs",
+        "--vocab_size",
+        default=10000,
+        type=int,
+        help="size of the vocabulary for the vectorizer",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_dir",
+        type=Path,
+        required=False,
+        help="where to write the output to, can be either a name for the folder wherein the model will be build (tf-model) or a .hdf5 file path",
+    )
+    args = parser.parse_args()
+    main(**vars(args))
